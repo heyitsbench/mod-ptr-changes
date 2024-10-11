012659477076aa860ae4d7433e1e7d644ab85e22
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-10-11 19:11:30 -0300

    Init module

---
+++ data/sql/db-world/updates/cc_ptr_commands.sql
@@ -0,0 +1,18 @@
+UPDATE `commands` SET `security` = 0 WHERE `name` IN (
+'aura', 'bags', 'bags clear', 'cast', 'cast back', 'cast dest', 'cast dist', 'cast self', 'cfbg', 'cfbg race',
+'cheat', 'cheat casttime',  'cheat cooldown', 'cheat explore', 'cheat god', 'cheat power', 'cheat taxi', 'cheat waterwalk',
+'combatstop', 'cooldown', 'damage', 'die', 'dismount', 'distance', 'gobject activate', 'gobject respawn', 'gobject add temp',
+'gps', 'help', 'maxskill', 'mailbox', 'npc add temp', 'npc info', 'recall', 'respawn', 'save', 'server info', 'unaura', 'unlearn', 'additem', 'additem set');
+
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "debug%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "event%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "gear%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "go%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "honor%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "instance%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "learn%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "list%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "lookup%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "modify%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "morph%";
+UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "quest%";
