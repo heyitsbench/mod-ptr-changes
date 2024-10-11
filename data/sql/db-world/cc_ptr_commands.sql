11f8feb353618bff65b96fb0c8a322e193372977
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-10-11 19:27:28 -0300

    fix folder

--- data/sql/db-world/cc_ptr_commands.sql
+++ data/sql/db-world/cc_ptr_commands.sql
@@ -2,7 +2,7 @@ UPDATE `commands` SET `security` = 0 WHERE `name` IN (
 'aura', 'bags', 'bags clear', 'cast', 'cast back', 'cast dest', 'cast dist', 'cast self', 'cfbg', 'cfbg race',
 'cheat', 'cheat casttime',  'cheat cooldown', 'cheat explore', 'cheat god', 'cheat power', 'cheat taxi', 'cheat waterwalk',
 'combatstop', 'cooldown', 'damage', 'die', 'dismount', 'distance', 'gobject activate', 'gobject respawn', 'gobject add temp',
-'gps', 'help', 'maxskill', 'mailbox', 'npc add temp', 'npc info', 'recall', 'respawn', 'save', 'server info', 'unaura', 'unlearn', 'additem', 'additem set');
+'gps', 'help', 'maxskill', 'mailbox', 'npc add temp', 'npc info', 'recall', 'respawn', 'save', 'server info', 'unaura', 'unlearn', 'additem', 'additem set', 'levelup');
 
 UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "debug%";
 UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "event%";
