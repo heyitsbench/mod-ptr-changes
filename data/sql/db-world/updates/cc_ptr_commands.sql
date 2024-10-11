9d2b8d982cd660689a33d7a245c174a0987ad6b0
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-10-11 19:46:08 -0300

    fix typos

--- data/sql/db-world/updates/cc_ptr_commands.sql
+++ data/sql/db-world/updates/cc_ptr_commands.sql
@@ -1,18 +1,18 @@
-UPDATE `commands` SET `security` = 0 WHERE `name` IN (
+UPDATE `command` SET `security` = 0 WHERE `name` IN (
 'aura', 'bags', 'bags clear', 'cast', 'cast back', 'cast dest', 'cast dist', 'cast self', 'cfbg', 'cfbg race',
 'cheat', 'cheat casttime',  'cheat cooldown', 'cheat explore', 'cheat god', 'cheat power', 'cheat taxi', 'cheat waterwalk',
 'combatstop', 'cooldown', 'damage', 'die', 'dismount', 'distance', 'gobject activate', 'gobject respawn', 'gobject add temp',
 'gps', 'help', 'maxskill', 'mailbox', 'npc add temp', 'npc info', 'recall', 'respawn', 'save', 'server info', 'unaura', 'unlearn', 'additem', 'additem set', 'levelup');
 
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "debug%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "event%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "gear%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "go%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "honor%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "instance%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "learn%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "list%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "lookup%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "modify%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "morph%";
-UPDATE `commands` SET `security` = 0 WHERE `name` LIKE "quest%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "debug%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "event%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "gear%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "go%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "honor%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "instance%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "learn%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "list%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "lookup%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "modify%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "morph%";
+UPDATE `command` SET `security` = 0 WHERE `name` LIKE "quest%";
---
+++ src/mod_ptr_changes_scripts.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ */
+
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Chat.h"
+
+void Addmod_ptr_changesScript()
+{
+}
