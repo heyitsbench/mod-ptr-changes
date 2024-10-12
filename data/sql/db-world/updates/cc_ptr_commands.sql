58a0279aa17404ddb90b07dde11304e40b8263eb
Andrew <47818697+Nyeriah@users.noreply.github.com>
2024-10-12 05:56:10 -0300

    Add teleport

--- data/sql/db-world/updates/cc_ptr_commands.sql
+++ data/sql/db-world/updates/cc_ptr_commands.sql
@@ -2,7 +2,8 @@ UPDATE `command` SET `security` = 0 WHERE `name` IN (
 'aura', 'bags', 'bags clear', 'cast', 'cast back', 'cast dest', 'cast dist', 'cast self', 'cfbg', 'cfbg race',
 'cheat', 'cheat casttime',  'cheat cooldown', 'cheat explore', 'cheat god', 'cheat power', 'cheat taxi', 'cheat waterwalk',
 'combatstop', 'cooldown', 'damage', 'die', 'dismount', 'distance', 'gobject activate', 'gobject respawn', 'gobject add temp',
-'gps', 'help', 'maxskill', 'mailbox', 'npc add temp', 'npc info', 'recall', 'respawn', 'save', 'server info', 'unaura', 'unlearn', 'additem', 'additem set', 'levelup');
+'gps', 'help', 'maxskill', 'mailbox', 'npc add temp', 'npc info', 'recall', 'respawn', 'save', 'server info', 'unaura', 'unlearn', 'additem', 'additem set', 
+'levelup', 'teleport');
 
 UPDATE `command` SET `security` = 0 WHERE `name` LIKE "debug%";
 UPDATE `command` SET `security` = 0 WHERE `name` LIKE "event%";
